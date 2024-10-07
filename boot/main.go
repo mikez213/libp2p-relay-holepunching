b7c5d8928de84d1b1e4ff5ec76385ae018155877
Mike Zhang <m.zhang@mawari.co.jp>
2024-10-07 15:46:38 -0400

    working server

--- boot/main.go
+++ boot/main.go
@@ -9,66 +9,69 @@ import (
 
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/peerstore"
-	"github.com/libp2p/go-libp2p/core/routing"
 
 	logging "github.com/ipfs/go-log/v2"
 	libp2p "github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/config"
 	multiaddr "github.com/multiformats/go-multiaddr"
 )
 
 var log = logging.Logger("bootstrap")
 
-var RelayerPrivateKeys = []string{
+// RelayerPrivateKeys holds the encoded private keys for relayers
+var relayerPrivateKeys = []string{
 	"CAESQAA7xVQKsQ5VAC5ge+XsixR7YnDkzuHa4nrY8xWXGK3fo9yN1Eaiat9Vn1iwaVQDqTjywVP303ojVLxXcQ9ze4E=",
-	//12D3KooWLr1gYejUTeriAsSu6roR2aQ423G3Q4fFTqzqSwTsMz9n
+	// 12D3KooWLr1gYejUTeriAsSu6roR2aQ423G3Q4fFTqzqSwTsMz9n
 	"CAESQMCYbjRpXBDUnIpDyqY+mA3n7z9gF3CaggWTknd90LauHUcz8ldNtlUchFATmMSE1r/NMnSpEBbLvzWQKq3N45s=",
-	//12D3KooWBnext3VBZZuBwGn3YahAZjf49oqYckfx64VpzH6dyU1p
+	// 12D3KooWBnext3VBZZuBwGn3YahAZjf49oqYckfx64VpzH6dyU1p
 	"CAESQB1Y1Li0Wd4KcvMvbv5/+CTG79axzl3R8yTuzWOckMgmNAzZqxim5E/7e9mgd87FTMPQNHqiItqTFwHJeMxr0H8=",
-	//12D3KooWDKYjXDDgSGzhEYWYtDvfP9pMtGNY1vnAwRsSp2CwCWHL
+	// 12D3KooWDKYjXDDgSGzhEYWYtDvfP9pMtGNY1vnAwRsSp2CwCWHL
 }
 
-const DefaultRelayerKeyIndex = 0
+const defaultRelayerKeyIndex = 0
 
-func RelayIdentity(keyIndex int) func(*config.Config) error {
-	return func(cfg *config.Config) error {
-		if keyIndex < 0 || keyIndex >= len(RelayerPrivateKeys) {
-			return fmt.Errorf("invalid key index: %d", keyIndex)
-		}
-		relayerPrivateKey := RelayerPrivateKeys[keyIndex]
-
-		b, err := crypto.ConfigDecodeKey(relayerPrivateKey)
-		if err != nil {
-			return fmt.Errorf("failed to decode relayer private key: %v", err)
-		}
-
-		priv, err := crypto.UnmarshalPrivateKey(b)
-		if err != nil {
-			return fmt.Errorf("failed to unmarshal private key: %v", err)
-		}
-		return cfg.Apply(libp2p.Identity(priv))
+// RelayIdentity returns a libp2p.Option to set the host's identity
+func RelayIdentity(keyIndex int) (libp2p.Option, error) {
+	if keyIndex < 0 || keyIndex >= len(relayerPrivateKeys) {
+		return nil, fmt.Errorf("invalid key index: %d", keyIndex)
 	}
+
+	relayerPrivateKey := relayerPrivateKeys[keyIndex]
+	keyBytes, err := crypto.ConfigDecodeKey(relayerPrivateKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode relayer private key: %w", err)
+	}
+
+	privKey, err := crypto.UnmarshalPrivateKey(keyBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal private key: %w", err)
+	}
+
+	return libp2p.Identity(privKey), nil
 }
 
 func main() {
-	logging.SetAllLoggers(logging.LevelInfo)
+	// Initialize logging
+	logging.SetAllLoggers(logging.LevelWarn)
 	logging.SetLogLevel("bootstrap", "debug")
 
+	// Parse command-line flags
 	listenPort := flag.Int("port", 1237, "TCP port to listen on")
 	bootstrapPeers := flag.String("bootstrap", "", "Comma-separated list of bootstrap peer multiaddresses")
-	keyIndex := flag.Int("key", DefaultRelayerKeyIndex, "Index of the RelayerPrivateKey to use")
+	keyIndex := flag.Int("key", defaultRelayerKeyIndex, "Index of the RelayerPrivateKey to use")
 	flag.Parse()
 
-	if *keyIndex < 0 || *keyIndex >= len(RelayerPrivateKeys) {
-		log.Fatalf("Invalid key index %d. max is %d", *keyIndex, len(RelayerPrivateKeys)-1)
+	// Set host identity
+	relayOption, err := RelayIdentity(*keyIndex)
+	if err != nil {
+		log.Fatalf("Failed to set relay identity: %v", err)
 	}
 
+	// Create a new libp2p host
 	ctx := context.Background()
 	host, err := libp2p.New(
-		RelayIdentity(*keyIndex),
+		relayOption,
 		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", *listenPort)),
 		libp2p.EnableRelay(),
 		libp2p.NATPortMap(),
@@ -76,34 +79,36 @@ func main() {
 		libp2p.EnableAutoNATv2(),
 		libp2p.EnableHolePunching(),
 		libp2p.ForceReachabilityPublic(),
-		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) { return dht.New(ctx, h, dht.Mode(dht.ModeServer)) }),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Infof("Bootstrap node is running. We are: %s", host.ID())
+	// Display host information
+	log.Infof("Bootstrap node is running. Peer ID: %s", host.ID())
 	log.Info("Listening on:")
 	for _, addr := range host.Addrs() {
 		log.Infof("%s/p2p/%s", addr, host.ID())
 	}
 
+	// Initialize and bootstrap the DHT in server mode
 	kademliaDHT, err := dht.New(ctx, host, dht.Mode(dht.ModeServer))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Debug("Bootstrapping the DHT")
-	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+	if err := kademliaDHT.Bootstrap(ctx); err != nil {
 		log.Fatal(err)
 	}
 
+	// Connect to bootstrap peers if provided
 	if *bootstrapPeers != "" {
-		peers := strings.Split(*bootstrapPeers, ",")
-		for _, peerAddr := range peers {
+		peerAddrs := strings.Split(*bootstrapPeers, ",")
+		for _, peerAddr := range peerAddrs {
 			peerAddr = strings.TrimSpace(peerAddr)
 			if peerAddr == "" {
-				log.Warnf("No addr")
+				log.Warn("Empty bootstrap peer address")
 				continue
 			}
 
@@ -119,9 +124,10 @@ func main() {
 				continue
 			}
 
-			// Add to peerstore
+			// Add peer addresses to the peerstore
 			host.Peerstore().AddAddrs(peerInfo.ID, peerInfo.Addrs, peerstore.PermanentAddrTTL)
 
+			// Attempt to connect to the bootstrap peer
 			if err := host.Connect(ctx, *peerInfo); err != nil {
 				log.Errorf("Failed to connect to bootstrap peer %s: %v", peerInfo.ID, err)
 				continue
@@ -130,14 +136,17 @@ func main() {
 		}
 	}
 
+	// Allow some time for connections to establish
 	time.Sleep(2 * time.Second)
 
+	// Display connection information
 	log.Infof("Running. Peer ID: %s", host.ID())
 	log.Info("Use multiaddresses to connect:")
 	for _, addr := range host.Addrs() {
 		log.Infof("%s/p2p/%s", addr, host.ID())
 	}
 
+	// Periodically log the current routing table peers
 	go func() {
 		for {
 			peers := kademliaDHT.RoutingTable().ListPeers()
@@ -146,5 +155,6 @@ func main() {
 		}
 	}()
 
+	// Keep the program running indefinitely
 	select {}
 }
