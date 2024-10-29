aae16568bd1bb745233cfd7be4bd4fa8980519f7
Mike Zhang <m.zhang@mawari.co.jp>
2024-10-29 10:56:23 -0400

    shared empty template

--- shared/shared.go
+++ shared/shared.go
@@ -1,9 +1,87 @@
 package shared
 
 import (
-	"log"
+	"fmt"
+
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
-func Printing(message string) {
-	log.Println("Printing from shared:", message)
+var log = logging.Logger("sharedlog")
+
+var bootstrapPeerIDs = []peer.ID{}
+
+var relayerPrivateKeys = []string{
+	//boots
+	"CAESQAA7xVQKsQ5VAC5ge+XsixR7YnDkzuHa4nrY8xWXGK3fo9yN1Eaiat9Vn1iwaVQDqTjywVP303ojVLxXcQ9ze4E=",
+	// pid: 12D3KooWLr1gYejUTeriAsSu6roR2aQ423G3Q4fFTqzqSwTsMz9n
+	"CAESQMCYbjRpXBDUnIpDyqY+mA3n7z9gF3CaggWTknd90LauHUcz8ldNtlUchFATmMSE1r/NMnSpEBbLvzWQKq3N45s=",
+	// pid: 12D3KooWBnext3VBZZuBwGn3YahAZjf49oqYckfx64VpzH6dyU1p
+	"CAESQB1Y1Li0Wd4KcvMvbv5/+CTG79axzl3R8yTuzWOckMgmNAzZqxim5E/7e9mgd87FTMPQNHqiItqTFwHJeMxr0H8=",
+	// pid: 12D3KooWDKYjXDDgSGzhEYWYtDvfP9pMtGNY1vnAwRsSp2CwCWHL
+
+	//relays
+	"CAESQHMEeM3iNIIxNThxIfnuO5FJ0oUQJy8V7TFD80lGziBE7SuPw2wckCrFRihVDaw0e6PkDCwsh/6u3UgBxB3OTFo=",
+	//12D3KooWRnBKUEkAEpsoCoEiuhxKBJ5j2Bdop6PGxFMvd4PwoevM
+	"CAESQP3Pu7TVp2RSVIZykj65/MDXm/eiTOfLGH3xCWQVmUoC67MkFWUEOd6QERl1Y4Xvi1Rt+d36UuaFXanT+hVUDAY=",
+	//12D3KooWRgSQnguL2DYkXUXqCLiRQ35PEX4eEH3havy2X18AVALd
+	"CAESQDE2IToG5mWwzWEeXt3/OVbx9XyE743DTenPFUG8M06IQXSarkNhuxNEJisnWeuDvaoaM/fNJNMqhPR81NL3Pio=",
+	//12D3KooWEDso33ti9KsKmD2g2egNmw6BXgch7V5vFz1TziuNYybo
+
+	//nodes
+	"CAESQFffsVM3eUXLozmXkBM2FSSVhEmo/Cq5RlXOAAaniTdCu3EQ6Zf7lQDasCj6IXyTihFQWZB+nmGFn/ZAA5y5egk=",
+	//12D3KooWNS4QQxwNURwoYoXmGjH9AQkagcGTjRUQT33P4i4FKQsi
+	"CAESQCSHrfyzNZkxwoNmXI1wx5Lvr6o4+kGxGepFH0AfYlKthyON+1hQRjLJQaBAQLrr1cfMHFFoC40X62DQIhL246U=",
+	//12D3KooWJuteouY1d5SYFcAUAYDVPjFD8MUBgqsdjZfBkAecCS2Y
+	"CAESQDyiSqC9Jez8wKSQs74YJalAegamjVKHbnaN35pfe6Gk21WVgCzfvBdLVoRj8XXny/k1LtSOhPZWNz0rWKCOYpk=",
+	//12D3KooWQaZ9Ppi8A2hcEspJhewfPqKjtXu4vx7FQPaUGnHXWpNL
+}
+
+func RelayIdentity(keyIndex int) (libp2p.Option, error) {
+	if keyIndex < 0 || keyIndex >= len(relayerPrivateKeys) {
+		return nil, fmt.Errorf("invalid key index: %d", keyIndex)
+	}
+
+	keyStr := relayerPrivateKeys[keyIndex]
+	keyBytes, err := crypto.ConfigDecodeKey(keyStr)
+	if err != nil {
+		return nil, fmt.Errorf("decode private key failed: %w", err)
+	}
+
+	privKey, err := crypto.UnmarshalPrivateKey(keyBytes)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal key failed: %w", err)
+	}
+
+	return libp2p.Identity(privKey), nil
+}
+
+func init() {
+	bootstrapIDStrs := []string{
+		"12D3KooWLr1gYejUTeriAsSu6roR2aQ423G3Q4fFTqzqSwTsMz9n",
+		"12D3KooWBnext3VBZZuBwGn3YahAZjf49oqYckfx64VpzH6dyU1p",
+		"12D3KooWDKYjXDDgSGzhEYWYtDvfP9pMtGNY1vnAwRsSp2CwCWHL",
+	}
+
+	for _, idStr := range bootstrapIDStrs {
+		pid, err := peer.Decode(idStr)
+		if err != nil {
+			log.Fatalf("failed to decode bootstrap pid '%s': %v", idStr, err)
+		}
+		bootstrapPeerIDs = append(bootstrapPeerIDs, pid)
+	}
+
+	logging.SetAllLoggers(logging.LevelInfo)
+	logging.SetLogLevel("chatlog", "debug")
+}
+
+func isBootstrapPeer(peerID peer.ID) bool {
+	for _, bootstrapID := range bootstrapPeerIDs {
+		if peerID == bootstrapID {
+			return true
+		}
+	}
+	return false
 }
